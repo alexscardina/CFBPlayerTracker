b9edf439a6b90b5a414bce4ac8394871690dca33
Alex Scardina <Alex@Alexs-MacBook-Pro-2.local>
2024-07-22 23:50:38 -0400

    modal changes

--- src/routes/Hub.svelte
+++ src/routes/Hub.svelte
@@ -1,7 +1,12 @@
 <script>
   import { v4 as uuid } from 'uuid';
+  import { Button, Modal } from 'flowbite-svelte';
   import Player from '../components/Player.svelte';
   import { players } from '../components/stores.js';
+
+  let formModalOpen = false;
+  let playerModalOpen = false;
+  let playerStatsModalOpen = false;
 
   let RTGPositions = ['QB', 'RB', 'WR', 'MLB', 'CB'];
 
@@ -38,29 +43,38 @@
   <h1>Road to Glory Players</h1>
 
   {#each $players as player}
-    <div class="player-card">
+    <div class="player-card" on:click={() => (playerModalOpen = true)}>
       <Player {player} />
       <button class="delete-button" on:click={() => deletePlayer(player.id)}>Delete Player</button>
     </div>
+    <Modal title="More Player Info" bind:open={playerModalOpen} outsideclose>
+      <!-- TODO: Make this its own component -->
+      <h4>{player.firstName}</h4>
+      <h3>{player.lastName}</h3>
+      <p>{player.position}</p>
+    </Modal>
   {/each}
 
   <!-- Form to add new RTG player -->
-  <form on:submit|preventDefault={addPlayer}>
-    <input bind:value={firstName} placeholder="First Name" />
-    <input bind:value={lastName} placeholder="Last Name" />
-    <select bind:value={position} placeholder="Position">
-      {#each RTGPositions as pos}
-        <option value={pos}>
-          {pos}
-        </option>
-      {/each}
-    </select>
-    <div class="row">
-      <p>Star Rating: </p>
-      <input bind:value={starRating} type="number" placeholder="Star Rating" />
-    </div>
-    <button class="add-button" type="submit">Add Player</button>
-  </form>
+  <Button on:click={() => (formModalOpen = true)}>Add New Player</Button>
+  <Modal title="Add New Player" bind:open={formModalOpen} autoclose outsideclose>
+    <form on:submit|preventDefault={addPlayer}>
+      <input bind:value={firstName} placeholder="First Name" />
+      <input bind:value={lastName} placeholder="Last Name" />
+      <select bind:value={position} placeholder="Position">
+        {#each RTGPositions as pos}
+          <option value={pos}>
+            {pos}
+          </option>
+        {/each}
+      </select>
+      <div class="row">
+        <p>Star Rating: </p>
+        <input bind:value={starRating} type="number" placeholder="Star Rating" />
+      </div>
+      <button class="add-button" type="submit">Add Player</button>
+    </form>
+  </Modal>
 </main>
 
 <style>
